5963d3d5c5b1a04db74716b4d032f6379b57cbda
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-30 15:47:40 +0900

    fix: top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 import Terminals from "../components/Terminals";
 import ArticleDescription from "../components/ArticleDescription";
-import BlogList from "../components/BlogList";
-import FetchArticles from "../components/FetchArticles";
 
 function Home() {
   return (
     <div>
       <Terminals />
-      <BlogList>
-        {({ initialBlogs }) => (
-          <div>
-            <ArticleDescription title="技術記事一覧" />
-            <FetchArticles initialBlogs={initialBlogs} />
-          </div>
-        )}
-      </BlogList>
+      <div>
+        <ArticleDescription title="技術記事一覧" />
+      </div>
     </div>
   );
 }
