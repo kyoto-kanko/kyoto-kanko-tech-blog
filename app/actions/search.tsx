57e004d0ca4e59f08bba22c8cce83c18bf2ed12b
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-07-08 01:48:41 +0900

    fix: search target title

--- app/actions/search.tsx
+++ app/actions/search.tsx
@@ -4,7 +4,7 @@ import client from "../../lib/apiClient";
 export default async function search(query: string) {
   const data = await client.get({
     endpoint: "blogs",
-    queries: { q: query },
+    queries: { filters: `title[contains]${query}` },
   });
   return data.contents;
 }
