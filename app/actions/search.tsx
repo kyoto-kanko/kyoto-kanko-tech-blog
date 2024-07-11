d9d54bbb2261b48caecc76703ffd2aa6fb85aec6
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-07-12 00:59:14 +0900

    feat: try catch

--- app/actions/search.tsx
+++ app/actions/search.tsx
@@ -2,9 +2,14 @@
 import client from "../../lib/apiClient";
 
 export default async function search(query: string) {
-  const data = await client.get({
-    endpoint: "blogs",
-    queries: { filters: `title[contains]${query}` },
-  });
-  return data.contents;
+  try {
+    const data = await client.get({
+      endpoint: "blogs",
+      queries: { filters: `title[contains]${query}` },
+    });
+    return data.contents;
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    return [];
+  }
 }
